46a9cba3b8526cda842fb0903f2daff8b2bd0d2c
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-07-22 18:07:23 +0200

    Add counter + reset button w/ state hook, adjust styles

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.counter}>{count}</Text>
+      <Button title="Count" onPress={() => setCount(count + 1)} />
+      <Button title="Reset" onPress={() => setCount(0)} />
     </View>
   );
 }
@@ -12,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'azure',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  counter: {
+    color: 'orange',
+    fontSize: 180,
   },
 });
