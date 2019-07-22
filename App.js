c23a65e50f4fe41dca6adab564438e5b8ee5d950
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-07-22 22:03:20 +0200

    Display FlatList component when adding items

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 
 export default function App() {
-  const [item, setItem] = useState(null);
+  const [items, setItems] = useState([]);
+
+  const _addItem = text => {
+    setItems([...items, { text, date: Date.now().toString() }]);
+  };
+
+  let content = <Text>Keine Einträge im Tagebuch</Text>;
+
+  if (items.length > 0) {
+    content = (
+      <FlatList
+        style={styles.list}
+        data={items}
+        renderItem={({ item }) => <Text>{item.text}</Text>}
+        keyExtractor={item => item.date}
+      />
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text>{item || 'Keine Einträge im Tagebuch'}</Text>
+      {content}
       <TextInput
         style={styles.input}
         placeholder="Tagebucheintrag erstellen"
         returnKeyType="done"
-        onSubmitEditing={event => setItem(event.nativeEvent.text)}
+        onSubmitEditing={event => _addItem(event.nativeEvent.text)}
       />
     </View>
   );
@@ -21,6 +38,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  list: {
+    marginTop: 24,
   },
   input: {
     height: 40,
