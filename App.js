ff5b55f26b5e13e5e81feb46f40f15b487310bb5
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-07-23 11:18:01 +0200

    Use SectionList to add items sectioned by date

--- App.js
+++ App.js
@@ -4,27 +4,72 @@ import {
   Text,
   View,
   TextInput,
-  FlatList,
+  SectionList,
   KeyboardAvoidingView,
 } from 'react-native';
 
+const journalItems = [
+  {
+    title: '29.7.2017',
+    data: [
+      {
+        text: 'Umgang mit SectionList in React Native gelernt',
+        date: 1,
+      },
+    ],
+  },
+  {
+    title: '28.7.2017',
+    data: [
+      {
+        text: 'Einkauf im Supermarkt',
+        date: 2,
+      },
+      {
+        text: 'Wochenendausflug geplant',
+        date: 3,
+      },
+    ],
+  },
+];
+
 export default function App() {
-  const [items, setItems] = useState([]);
-  const inputEl = useRef(null);
+  const [items, setItems] = useState(journalItems);
+  const textInput = useRef(null);
 
   const _addItem = text => {
-    setItems([...items, { text, date: Date.now().toString() }]);
-    inputEl.current.clear();
+    let [head, ...tail] = items;
+
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth() + 1;
+    const year = now.getFullYear();
+    const today = `${day}.${month}.${year}`;
+
+    if (head === undefined || head.title !== today) {
+      head = { title: today, data: [] };
+      tail = items;
+    }
+
+    const newItem = { text, date: now.getTime() };
+    head.data = [newItem, ...head.data];
+    const newItems = [head, ...tail];
+
+    setItems(newItems);
+    textInput.current.clear();
   };
 
   let content = <Text>Keine Einträge im Tagebuch</Text>;
 
   if (items.length > 0) {
     content = (
-      <FlatList
+      <SectionList
         style={styles.list}
-        data={items}
+        sections={items}
         renderItem={({ item }) => <Text>{item.text}</Text>}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.listHeader}>{section.title}</Text>
+        )}
         keyExtractor={item => item.date}
       />
     );
@@ -36,7 +81,7 @@ export default function App() {
       <KeyboardAvoidingView behavior="padding">
         <TextInput
           style={styles.input}
-          ref={inputEl}
+          ref={textInput}
           placeholder="Tagebucheintrag erstellen"
           returnKeyType="done"
           onSubmitEditing={event => _addItem(event.nativeEvent.text)}
@@ -53,6 +98,9 @@ const styles = StyleSheet.create({
   },
   list: {
     marginTop: 24,
+  },
+  listHeader: {
+    backgroundColor: 'darkgray',
   },
   input: {
     height: 40,
