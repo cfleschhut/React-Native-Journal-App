ee338685fc796a94b3c49c8868ce7b459c233596
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-08-08 21:52:23 +0200

    Save/Load items to/from AsyncStorage

--- App.js
+++ App.js
@@ -1,60 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import JournalItems from './js/components/JournalItems';
 import JournalItemInput from './js/components/JournalItemInput';
-
-const journalItems = [
-  {
-    title: '29.7.2017',
-    data: [
-      {
-        text: 'Umgang mit SectionList in React Native gelernt',
-        date: 1501322400000,
-      },
-    ],
-  },
-  {
-    title: '28.7.2017',
-    data: [
-      {
-        text: 'Einkauf im Supermarkt',
-        date: 1501236060000,
-      },
-      {
-        text: 'Wochenendausflug geplant',
-        date: 1501236000000,
-      },
-    ],
-  },
-];
+import Store from './js/Store';
 
 export default function App() {
-  const [items, setItems] = useState(journalItems);
+  const [items, setItems] = useState([]);
+
+  const _refreshItems = async () => {
+    const items = await Store.loadItems();
+    setItems(items);
+  };
+
+  useEffect(() => {
+    _refreshItems();
+  }, []);
+
+  const _getSectionTitleFromDate = date => {
+    const dateObj = new Date(date);
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() + 1;
+    const year = dateObj.getFullYear();
+    return `${day}.${month}.${year}`;
+  };
+
+  const _getItemsWithSections = items => {
+    if (!items.length) return [];
+
+    let sectionTitle = _getSectionTitleFromDate(items[0].date);
+    let sections = [{ title: sectionTitle, data: [] }];
+
+    items.forEach(item => {
+      sectionTitle = _getSectionTitleFromDate(item.date);
+      let lastSection = sections[sections.length - 1];
+
+      lastSection.title !== sectionTitle
+        ? sections.push({ title: sectionTitle, data: [item] })
+        : lastSection.data.push(item);
+    });
+
+    return sections;
+  };
 
   const _addItem = (text, photo) => {
-    let [head, ...tail] = items;
-
-    const now = new Date();
-    const day = now.getDate();
-    const month = now.getMonth() + 1;
-    const year = now.getFullYear();
-    const today = `${day}.${month}.${year}`;
-
-    if (head === undefined || head.title !== today) {
-      head = { title: today, data: [] };
-      tail = items;
-    }
-
-    const newItem = { text, photo, date: now.getTime() };
-    head.data = [newItem, ...head.data];
-    const newItems = [head, ...tail];
+    const newItems = [
+      {
+        text,
+        photo,
+        date: Date.now(),
+      },
+      ...items,
+    ];
 
     setItems(newItems);
+    Store.saveItems(newItems);
   };
+
+  const sections = _getItemsWithSections(items);
 
   return (
     <View style={styles.container}>
-      <JournalItems items={items} />
+      <JournalItems items={sections} />
       <JournalItemInput onSubmit={(text, photo) => _addItem(text, photo)} />
     </View>
   );
