8158e9ece852fbc14006287593084afd3d88f26e
Christian Fleschhut <weltrekordhalter@gmail.com>
2019-09-02 22:56:41 +0200

    Add button to delete all journal entries to settings screen

--- js/screens/SettingsScreen.js
+++ js/screens/SettingsScreen.js
@@ -1,12 +1,39 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Button, Alert, StyleSheet } from 'react-native';
+import Store from '../Store';
 
-export default class SettingsScreen extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings</Text>
-      </View>
+export default function SettingsScreen({ screenProps }) {
+  const _deleteItems = () => {
+    Alert.alert(
+      'Delete entries',
+      'Do you want to delete all journal entries?',
+      [
+        {
+          text: 'No',
+          style: 'cancel',
+        },
+        {
+          text: 'Yes',
+          onPress: async () => {
+            await Store.deleteItems();
+            screenProps.refresh();
+          },
+        },
+      ],
     );
-  }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Delete all entries" onPress={() => _deleteItems()} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
